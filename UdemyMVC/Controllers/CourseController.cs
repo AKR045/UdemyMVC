4c66608945a76ba9955fd91f21b2b230f1fc6d2c
Ahmed Kamel <ahmedkamel19992021@gmail.com>
2024-08-11 05:47:24 +0300

    Overall Rating Error & Chapters

--- UdemyMVC/Controllers/CourseController.cs
+++ UdemyMVC/Controllers/CourseController.cs
@@ -24,11 +24,11 @@ namespace UdemyMVC.Controllers
             {
                 return NotFound();
             }
-            decimal sumRateAll = course.CourseRates.Select(c => c.Rate).Sum();
-            int countRateAll = course.CourseRates.Select(c => c.Rate).Count();
+            decimal sumRateAll = courses.Where(c => c.CourseRates != null).SelectMany(c => c.CourseRates).Sum(r=>r.Rate);
+            int countRateAll = courses.Where(c => c.CourseRates != null).SelectMany(c => c.CourseRates).Select(r=>r.Rate).Count();
 
-            decimal sumRate = course.CourseRates.Where(c=>c.CourseID==id).Select(c => c.Rate).Sum();
-            int countRate = course.CourseRates.Where(c => c.CourseID == id).Select(c => c.Rate).Count();
+            decimal sumRate = course.CourseRates?.Sum(c => c.Rate)??0;
+            int countRate = course.CourseRates?.Select(c => c.Rate).Count()??0;
             CourseDetailsVM courseVM = new CourseDetailsVM();
             courseVM.Id = course.ID;
             courseVM.CourseName = course.CourseName;
@@ -38,7 +38,7 @@ namespace UdemyMVC.Controllers
             courseVM.Price = course.Price;
             //courseVM.Chapters = course.Chapters.ToList();
             //courseVM.Topics = course.Chapters.Select(c=>c.Topics).ToString();
-            courseVM.Rating = countRate > 0 ? (sumRate / countRate).ToString() : "1";
+            courseVM.Rating = countRate > 0 ? (sumRate / countRate).ToString("0.0") : "1";
             courseVM.NumberOfRating = countRate;
             courseVM.NumberOfStudent = course.Enrollment.Count();
             courseVM.Instructor = course.Instructor.User.FullName;
@@ -49,8 +49,8 @@ namespace UdemyMVC.Controllers
             courseVM.InstructorIamge=course.Instructor.User.Image;
             courseVM.InstructorAllCourses = courses.ToList();
             courseVM.InstructorAllCoursesNumber= courses.Count();
-            courseVM.InstructorAllStudentsNumber= courses.Select(c=>c.Enrollment).Distinct().Count();
-            courseVM.AllCourseRate = countRateAll > 0 ? (sumRateAll / countRateAll).ToString():"1";
+            courseVM.InstructorAllStudentsNumber = courses.Where(c => c.Enrollment != null).SelectMany(c => c.Enrollment).Select(s => s.UserID).Distinct().Count();
+            courseVM.AllCourseRate = countRateAll > 0 ? (sumRateAll / countRateAll).ToString("0.0") :"1";
             
             return View("CourseDetailes", courseVM);
         }
